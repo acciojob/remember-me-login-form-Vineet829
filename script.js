9ceabea1b8ddc7968d83f7b7b6e4ca45c101926b
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-28 13:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+let btn = document.querySelector("#submit");
+let exist = document.querySelector("#existing");
+let check = document.querySelector("#checkbox");
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (check.checked) {
+        localStorage.setItem("username", username.value);
+        localStorage.setItem("password", password.value);
+    } else {
+        localStorage.removeItem("username");
+        localStorage.removeItem("password");
+    }
+
+    alert("Logged in as " + username.value);
+    updateUserDisplay();
+});
+
+const updateUserDisplay = () => {
+    if (localStorage.getItem("username")) {
+        exist.style.display = "block";
+    } else {
+        exist.style.display = "none";
+    }
+};
+
+updateUserDisplay();
+
+exist.addEventListener("click", (e) => {
+    e.preventDefault();
+    const existingUsername = localStorage.getItem("username");
+    if (existingUsername) {
+        alert("Logged in as " + existingUsername);
+    }
+});
+
